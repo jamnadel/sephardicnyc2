1364c0211495461e8d1b6af15e1ed7f538bc43e8
Jamie <jamnadel@umich.edu>
2020-04-23 11:32:41 -0400

    initial import

--- map.js
+++ map.js
@@ -1,3 +1,38 @@
 'use strict'
 
 console.log('Loaded map.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFtbmFkZWwiLCJhIjoiY2s4NmdpMW93MGhicjNscnEyNTVzNDQzbCJ9.duijjHslyNDxEtAfMBvh-w'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/jamnadel/ck9c3bg4506ei1ilu2hogirdt',
+    center: [-73.967925,40.772752],
+    zoom: 10
+})
+
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+map.addControl(navigation, 'top-left')
+
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+map.addControl(scale, 'bottom-right')
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+map.addControl(geolocate, 'top-left')
+
+geolocate.on('geolocate', function(event) {
+
+})
